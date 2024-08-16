6b7b36220f589f9520270f084ac977292c5e5b43
Fabiana Bachur <fabianabachur@hotmail.com>
2024-08-16 14:08:08 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/fabianabachur/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.fabianabachur.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabianabachur.cursomc.domain.Categoria;
+import com.fabianabachur.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
