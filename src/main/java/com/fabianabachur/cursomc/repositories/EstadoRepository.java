d13e548160c8c0684c8950d6cf40c25ddb018773
Fabiana Bachur <fabianabachur@hotmail.com>
2024-08-18 13:55:02 -0300

    Estado e Cidade

---
+++ src/main/java/com/fabianabachur/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.fabianabachur.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fabianabachur.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+}
